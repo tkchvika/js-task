ba39897ae5fef65596f7827f2776d053cfb7557d
unknown <t.vika050406@gmail.com>
2022-09-05 11:31:13 +0300

    add string4.js

--- string3.js
+++ string3.js
@@ -1,7 +1,7 @@
-const a = 2;
-const b = 3;
+// const a = 2;
+// const b = 3;
 
-console.log(String('це цифри') + (' ') + (String('a =2, b = 3')));
-console.log(String('сума цифр дорівнює') + (' ') + (a + b));
+// console.log(String('це цифри') + (' ') + (String('a = 2, b = 3')));
+// console.log(String('сума цифр дорівнює') + (' ') + (a + b));
 
-// done!
+ // done!
---
+++ string4.js
@@ -0,0 +1,9 @@
+let num = window.prompt("enter a nuber", "0");
+num = Number(num);
+
+console.log(num + 7);
+console.log(num - 9);
+
+// done!
+
+
