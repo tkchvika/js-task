f66001a9ecb1cfd72be29f1d338bd42f9c2c87dd
unknown <t.vika050406@gmail.com>
2022-09-12 20:14:39 +0300

    add string20.js

---
+++ string20.js
@@ -0,0 +1,15 @@
+// var num = 47;
+// num = num + 7;
+// num = num - 18;
+// num = num * 10;
+// num = num / 15;
+// alert(num);
+
+let num = 47;
+num += 7;
+num -= 18;
+num *= 10;
+num /= 15;
+alert(num);
+
+//done!
