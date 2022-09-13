243bc058acc912eeb0273199debdf840f8f3feac
unknown <t.vika050406@gmail.com>
2022-09-13 21:16:59 +0300

    add string28.js

--- string27.js
+++
@@ -1,7 +0,0 @@
-let re = /@/gi;
-let str = 'aaa@bbb@ccc';
-let newstr = str.replace(re, '!');
-
-console.log(newstr);
-
-//done!
---
+++ string28.js
@@ -0,0 +1,7 @@
+let re = /@/gi;
+let str = 'aaa@bbb@ccc';
+let newstr = str.replace(re, '!');
+
+console.log(newstr);
+
+//done!
