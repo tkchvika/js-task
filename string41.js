c51d40a61fa9c0df01878893bc9f37f64c742234
unknown <t.vika050406@gmail.com>
2022-09-19 18:22:11 +0300

    add string41.js

---
+++ string41.js
@@ -0,0 +1,9 @@
+function deleteFirstAndLastSymbole(str) {
+	str = str.slice(1, -1);
+	return str
+}
+
+console.log(deleteFirstAndLastSymbole('vika'));
+console.log(deleteFirstAndLastSymbole('batman'));
+
+//done!
