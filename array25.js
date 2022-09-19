6b2e7a66a602a10cf3ac121a34492b1465712517
unknown <t.vika050406@gmail.com>
2022-09-19 20:18:27 +0300

    add

---
+++ array25.js
@@ -0,0 +1,9 @@
+const words = ["Rayn", "Киран", "Джейсон", "Юс"];
+
+function yourFriends(arr) {
+	result = words.filter(word => word.length <= 4);
+	return result
+}
+console.log(yourFriends());
+
+//done!
