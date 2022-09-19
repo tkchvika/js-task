9708af3410bcbdabd15d1f3d8c342411afc78406
unknown <t.vika050406@gmail.com>
2022-09-19 18:36:50 +0300

    add string42.js

---
+++ string42.js
@@ -0,0 +1,9 @@
+function capitalLetter(str) {
+	str1 = str.charAt(0).toUpperCase() + str.slice(1);
+	return str1
+}
+
+console.log(capitalLetter('tapichok'));
+console.log(capitalLetter('tara'));
+
+//done!
