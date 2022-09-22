99890a246d4c17fde508a9cb44241e2096d8e3c4
unknown <t.vika050406@gmail.com>
2022-09-22 13:53:07 +0300

    add string53.js

---
+++ string53.js
@@ -0,0 +1,12 @@
+let a;
+let b;
+a = parseFloat(window.prompt('Enter first number'));
+b = parseFloat(window.prompt('Enter first number'));
+
+if (a == b) {
+	alert('numbers are equal');
+} else {
+	alert('numbers are not equal');
+}
+
+//done!
