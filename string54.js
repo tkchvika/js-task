b7322ed4de5ced10c0747bd4371a4b4d70fc4d03
unknown <t.vika050406@gmail.com>
2022-09-22 14:03:51 +0300

    add string54.js

---
+++ string54.js
@@ -0,0 +1,12 @@
+let desks;
+let students;
+desks = parseFloat(window.prompt('number of desks'));
+students = parseFloat(window.prompt('number of students'));
+
+if (desks >= students) {
+	alert('its okay');
+} else {
+	alert('not okay');
+}
+
+//done!
