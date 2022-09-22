10dc9f49ef8ec113a02a25b00ea04d9605db6da9
unknown <t.vika050406@gmail.com>
2022-09-22 13:06:53 +0300

    add string50.js

---
+++ string50.js
@@ -0,0 +1,17 @@
+let a;
+let b;
+a = parseFloat(prompt('Enter first number'));
+b = parseFloat(prompt('Enter second number'));
+if (a == b) {
+	alert('The same sign');
+}
+if (a < b) {
+	alert('Different sign');
+} else if (a > b) {
+	alert('Different sign');
+}
+if (a == 0 && b == 0) {
+	alert('Error');
+}
+
+//done!
