f639bfef0478d140b824f4faf574754ef028c511
unknown <t.vika050406@gmail.com>
2022-09-22 13:32:18 +0300

    add string51.js

---
+++ string51.js
@@ -0,0 +1,13 @@
+
+let a;
+let b;
+a = parseFloat(window.prompt('Enter first number'));
+b = parseFloat(window.prompt('Enter second number'));
+if (a / b > 0) {
+	window.alert('positive fraction');
+}
+if (a / b < 0) {
+	window.alert('negative fraction')
+}
+
+//done!
