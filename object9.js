77425f0fea1dd1d8b12a7a94ffdf3425fac2c3bc
unknown <t.vika050406@gmail.com>
2022-09-22 19:56:13 +0300

    add object9

---
+++ object9.js
@@ -0,0 +1,13 @@
+function isEmpty(object) {
+	const objectKeys = Object.keys(object);
+	if (objectKeys.length === 0) {
+		return true;
+	}
+	return !objectKeys.filter((key) => object[key] || object[key] === 0 || object[key] === false).length;
+};
+
+console.log(isEmpty({}));
+console.log(isEmpty({ a: undefined }));
+console.log(isEmpty({ a: 1 }));
+
+//done!
