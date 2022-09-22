e534a1149e65490d05a335b76ed723ec3ea73901
unknown <t.vika050406@gmail.com>
2022-09-22 12:24:15 +0300

    add string48.js

---
+++ string48.js
@@ -0,0 +1,14 @@
+let num;
+num = parseFloat(window.prompt('Enter a number!'));
+
+if (num > 0) {
+	alert('positive');
+}
+if (num == 0) {
+	alert('null');
+}
+if (num < 0) {
+	alert('negative');
+}
+
+//done!
