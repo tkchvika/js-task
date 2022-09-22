8cde67aa60e7be01ad3e8901ff0f8a563f862143
unknown <t.vika050406@gmail.com>
2022-09-22 12:53:50 +0300

    add string49.js

---
+++ string49.js
@@ -0,0 +1,15 @@
+let a;
+let b;
+a = parseFloat(prompt('Enter first number'));
+b = parseFloat(prompt('Enter second number'));
+if (a > b) {
+	alert(a + b);
+}
+if (a < b) {
+	alert(a * b);
+}
+if (a == b) {
+	alert('Numbers are equal!');
+}
+
+//DONE!
