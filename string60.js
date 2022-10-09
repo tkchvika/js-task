337d4c63bdcd45d8f33f081ba167c0c9e01bbb81
unknown <t.vika050406@gmail.com>
2022-10-09 20:50:51 +0300

    add string60.js

---
+++ string60.js
@@ -0,0 +1,12 @@
+let chairs;
+let students;
+chairs = parseFloat(window.prompt('number of chairs'));
+students = parseFloat(window.prompt('number of students'));
+
+if (chairs >= students) {
+	alert('its okay');
+} else {
+	alert('not okay');
+}
+
+//done!
