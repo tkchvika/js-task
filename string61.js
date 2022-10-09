6568371c5ce44053e012abbe50044c4a65b76397
unknown <t.vika050406@gmail.com>
2022-10-09 21:16:15 +0300

    add string61.js

---
+++ string61.js
@@ -0,0 +1,20 @@
+let num = prompt("enter a mark");
+function isInteger(num) {
+
+	return (num ^ 0) === num;
+}
+
+
+// if () {
+// 	alert('okay');
+// } else {
+// 	alert('error')
+// }
+//як зробити alert тут?
+
+
+// console.log(isInteger(3.4));
+// console.log(isInteger(3));
+// console.log(isInteger(4));
+
+//done
